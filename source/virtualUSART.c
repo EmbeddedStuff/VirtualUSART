deeded2470a67fc44f49482801fac62c2196a43a
Manuel Limones <limonesu.me@gmail.com>
2015-07-27 00:22:44 -0500

    Add register initialization for virtual usart

--- source/virtualUSART.c
+++ source/virtualUSART.c
@@ -5,6 +5,23 @@
 VUSART_t *  newVUSART(void (*isr_handler)(void)){
 	VUSART_t * vusart;
 	vusart = malloc(sizeof(VUSART_t));
+	
+	VUSART_REGISTER_CR_t * cr=&(vusart->registers.CR);
+	VUSART_REGISTER_ISR_t * isr=&(vusart->registers.ISR);
+	
+	/*Default status flags values*/
+	isr->LBD=0;
+	isr->TXE=1;
+	isr->TC=1;
+	isr->RXNE=0;
+	
+	/*All interrupts disabled by default*/
+	cr->LBDIE=0;
+	cr->RXNEIE=0;
+	cr->SBKRQ=0;
+	cr->TCIE=0;
+	cr->TXEIE=0;
+	
 	vusart->isr_handler=isr_handler;
 	return vusart;
 }
@@ -25,8 +42,16 @@ char VUSART_Method_ReadData(VUSART_t * vusart){
 void external_sendData(VUSART_t * vusart,char data){
 	vusart->registers.RX=data;
 	vusart->registers.ISR.RXNE=1;
-	vusart->isr_handler();
 }
 
 void VUSART_StepSimualtion(VUSART_t * vusart){
+	/*Interrupt Generation*/
+	VUSART_REGISTER_CR_t * cr=&(vusart->registers.CR);
+	VUSART_REGISTER_ISR_t * isr=&(vusart->registers.ISR);
+	
+	/*Interrupt generation*/
+	if(((isr->TXE)&&(cr->TXEIE))||
+		((isr->LBD)&&(cr->LBDIE))||
+		((isr->RXNE)&&(cr->RXNEIE))||
+		((isr->TC)&&(cr->TCIE))) vusart->isr_handler();
 }
